3a22aa8e4bb5ebbddde9306870c48adfb3c46792
Silvio Geoffroy <sageoffroy@gmail.com>
2026-09-10 18:50:46 -0300

    feat: run self-update check at startup

--- src/AdventurerLauncher/App.xaml.cs
+++ src/AdventurerLauncher/App.xaml.cs
@@ -1,7 +1,26 @@
 using System.Windows;
+using AdventurerLauncher.Services;
 
 namespace AdventurerLauncher;
 
 public partial class App : Application
 {
+    private static readonly HttpClient HttpClient = new()
+    {
+        Timeout = TimeSpan.FromMinutes(10)
+    };
+
+    private async void Application_Startup(object sender, StartupEventArgs e)
+    {
+        var selfUpdater = new SelfUpdateService(HttpClient);
+        if (await selfUpdater.TryStartUpdateAsync())
+        {
+            Shutdown();
+            return;
+        }
+
+        var window = new MainWindow();
+        MainWindow = window;
+        window.Show();
+    }
 }
